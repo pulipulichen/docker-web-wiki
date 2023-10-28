69c5e5115511f7b4f415d2b4ded0e3f397a1cc3b
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-10-29 04:18:17 +0800

    西元2023年10月29日 (週日) 04時18分17秒 CST

--- udocker.sh
+++ udocker.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-udocker --allow-root run -p 8000:80 --volume=/content/runtime:/var/www/html/ pudding/docker-web:pwiki-20231029-0337 &
+mkdir -p /content/runtime
+udocker --allow-root run -p 8000:80 --volume=/content/runtime:/var/www/html/ pudding/docker-web:pwiki-20231029-0337
