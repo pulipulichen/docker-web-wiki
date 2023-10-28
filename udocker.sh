2b59fb32d2036ffc7495de7598b05900149e3c86
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-10-29 05:03:11 +0800

    西元2023年10月29日 (週日) 05時03分11秒 CST

--- udocker.sh
+++ udocker.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+# 使用說明
+# - 對外port必須是8000
+# - 如用到目錄，必須事前建立
+# - 目錄路徑必須是完整路徑
+
 mkdir -p /content/runtime
-nohup udocker --allow-root run -p 8000:80 --volume=/content/runtime:/var/www/html/ pudding/docker-web:pwiki-20231029-0338 &
+nohup udocker --allow-root run -p 8000:80 --volume=/content/runtime:/var/www/html/ pudding/docker-web:pwiki-20231029-0338 > .nohup.out 2>&1 &
+
+
